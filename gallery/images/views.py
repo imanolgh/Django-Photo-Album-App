a52b8b5e4057e02bf88135a035fdc4285f795e31
onomy <onomy@LAPTOP-7JTKALOB>
2022-04-25 01:20:46 -0400

    4/24

--- gallery/images/views.py
+++ gallery/images/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from .models import images, albums
 
 def hub(request):
-    return render(request, 'images/hub.html')
-
-def view_image(request, primary):
-    return render(request, 'images/view_image.html')
+    image_for_gallery = images.objects.all()
+    album_names = albums.objects.all()
+    all_albums = {'image_for_gallery': image_for_gallery, 'album_names': album_names}
+    return render(request, 'images/hub.html', all_albums)
 
 def upload_image(request):
-    return render(request, 'images/upload_image.html')
+    album_names = albums.objects.all() 
+    return render(request, 'images/upload_image.html',  {'album_names': album_names})
 
-def album(request):
-    albums = images.objects.all()
-    context = {'albums': albums}
-    return render(request, 'images/album.html', context)
+def view_image(request, primary):
+    image_to_view = images.objects.get(id=primary)
+    view = {'image_to_view': image_to_view}
+    return render(request, 'images/view_image.html', view)
+
