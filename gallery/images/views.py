f0d2728bf437af9f2421a7c099306bc82ca1f979
onomy <onomy@LAPTOP-7JTKALOB>
2022-04-25 22:07:57 -0400

    4/25

--- gallery/images/views.py
+++ gallery/images/views.py
@@ -4,10 +4,28 @@ from .models import images, albums
 def hub(request):
     image_for_gallery = images.objects.all()
     album_names = albums.objects.all()
+    album = request.GET.get('album')
+    
+    if album != None:
+        image_for_gallery = images.objects.filter(album__album_name=album)
+    else:
+        image_for_gallery = images.objects.all()
+
     all_albums = {'image_for_gallery': image_for_gallery, 'album_names': album_names}
     return render(request, 'images/hub.html', all_albums)
 
 def upload_image(request):
+    if request.method == 'POST':
+        img = request.FILES.get("img")
+        formFields = request.POST
+
+        if formFields['new_album'] != "":
+            album, created = albums.objects.get_or_create(album_name = formFields["new_album"])
+        else:
+            album = albums.objects.get(id = formFields['album'])
+        
+        image = images.objects.create(album = album, caption = formFields["caption"], gallery_image = img)
+
     album_names = albums.objects.all() 
     return render(request, 'images/upload_image.html',  {'album_names': album_names})
 
